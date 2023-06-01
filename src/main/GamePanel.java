b04f976a93d7578f0486e6735922a0eefc0b8828
palanjian <ppalanji@usc.edu>
2023-05-31 19:56:53 -0700

    Initial commit

---
+++ src/main/GamePanel.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+	
+	//screen settings
+	final int originalTileSize = 16; //16x16 tile
+	final int scale = 3;
+	
+	final int tileSize = originalTileSize * scale; //48x48
+	final int maxScreenCol = 16;
+	final int maxScreenRow = 12;
+	final int screenWidth = tileSize * maxScreenCol; //768 pixels
+	final int screenHeight = tileSize * maxScreenRow; //576 pixels
+	
+	public GamePanel() {
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true); //all drawing will be done in an offscreen painting buffer
+		
+		
+	}
+	
+}
