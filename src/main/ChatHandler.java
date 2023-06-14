70ef38f91cc4c6da35cebe66f6bd47475260c12d
palanjian <ppalanji@usc.edu>
2023-06-13 21:08:56 -0700

    New chat with guild support and world support, before worlds though, holding off on this for now

--- src/main/ChatHandler.java
+++ src/main/ChatHandler.java
@@ -1,6 +1,12 @@
 package main;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.Graphics2D;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayDeque;
 
 import packets.ChatMessage;
@@ -9,23 +15,58 @@ public class ChatHandler {
 	private GamePanel gamePanel;
 	private ArrayDeque<ChatMessage> messageQueue;
 	
-	private int FONT_SIZE = 30; //30px
+	private int FONT_SIZE = 16; //px
+	private int MAX_MESSAGES = 8;
+	private Font FONT;
 	
 	public ChatHandler(GamePanel gamePanel) {
 		this.gamePanel = gamePanel;
 		this.messageQueue = new ArrayDeque<ChatMessage>();
+		try {
+			FONT = Font.createFont(Font.TRUETYPE_FONT, new File("res/" + gamePanel.getFontFileName() + ".ttf"));
+			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		    ge.registerFont(FONT);
+		}
+		catch(IOException | FontFormatException e) {
+			System.out.println("Error loading chat font.");
+			System.exit(0);
+		}
 	}
 
 	public void draw(Graphics2D g2) {
 		int msgNumber = 1;
 		for(ChatMessage msg : messageQueue) {
-			g2.drawString(msg.getMessage(), gamePanel.tileSize / 2, gamePanel.screenHeight - (gamePanel.tileSize / 2) - msgNumber*FONT_SIZE);
+			String formatted = "";
+			g2.setFont(new Font(FONT.getFontName(), Font.PLAIN, FONT_SIZE));
+			
+			//make more elegant
+			switch(msg.getRecipient()) {
+			case("all"):
+				formatted += "[Server] " + msg.getMessage();
+				g2.setColor(new Color(209, 85, 80));
+				break;
+			case("world"):
+				formatted += "[World] " + msg.getSender() + ": " + msg.getMessage(); //can change to player.getWorldName() once worlds are implemented
+				g2.setColor(Color.WHITE);
+				break;
+			case("guild"):
+				formatted += "[Guild] " + msg.getSender() + ": " + msg.getMessage();
+				g2.setColor(new Color(37, 127, 39));
+				break;
+			case("whisper"):
+				formatted += "[Whisper] " + msg.getSender() + ": " + msg.getMessage();
+				g2.setColor(new Color(207, 159, 255));;
+				g2.setFont(new Font(FONT.getFontName(), Font.ITALIC, FONT_SIZE));
+				break;
+			}		
+			
+			g2.drawString(formatted, gamePanel.tileSize / 2, gamePanel.screenHeight - (gamePanel.tileSize / 2) - msgNumber*FONT_SIZE);
 			++msgNumber;
 		}
 	}
 	
 	public void service(ChatMessage message) {
-		if(messageQueue.size() == 6) {
+		if(messageQueue.size() == MAX_MESSAGES) {
 			messageQueue.removeLast();
 		}
 		messageQueue.addFirst(message);
--- src/packets/ChatMessage.java
+++ src/packets/ChatMessage.java
@@ -7,17 +7,22 @@ public class ChatMessage implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private String message;
-	private long timeSent;
-	private String recipient; //"world", "guild", "whisper"
-	public ChatMessage(String message, String recipient){
+	private String sender;
+	private String messageType;
+	private String recipient; //"all", "world", "guild", "whisper"
+	
+	public ChatMessage(String message, String sender, String messageType){
 		this.message = message;
-		this.recipient = recipient;
+		this.sender = sender;
+		this.messageType = messageType;
 	}
 	
 	public String getMessage() { return message; }
 	public void setMessage(String message) { this.message = message; }
 	public String getRecipient() { return recipient; }
 	public void setRecipient(String recipient) { this.recipient = recipient; }
-	public long getTimeSent() { return timeSent; }
-	public void setTimeSent(long timeSent) { this.timeSent = timeSent; }
+	public String getSender() { return sender; }
+	public void setSender(String sender) { this.sender = sender; }
+	public String getMessageType() { return messageType; }
+	public void setMessageType(String messageType) { this.messageType = messageType; }
 }
